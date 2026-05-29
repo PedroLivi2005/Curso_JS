70bf6226904c660db0f650153f23e7cbb6debc1b
Pedro Livi <pedro.livi2005@gmail.com>
2026-05-28 22:32:09 -0300

    aulas10 e 11

---
+++ aula11/ex009.js
@@ -0,0 +1,17 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é Estrangeiro!')
+} else {
+    console.log('Você é Brasileiro!')
+}
+
+/*
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
+*/
